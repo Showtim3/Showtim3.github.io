84ae06851911767a9bafe77c5210fcc20ce6533a
Showtim3 <coolrishabhrawat@gmail.com>
2019-09-29 15:40:03 +0530

    Added landing page

--- index.js
+++ index.js
@@ -12,11 +12,20 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.collapsible').collapsible();
 
-
-
-
-
     // $('.ml2').each(function(){
     // $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
 });
+$(window).on('load',function() {
+    // executes when complete page is fully loaded, including all frames, objects and images
+    // $('#portfolio').style.display = 'block';
+    // $('#introBox').style.display = 'none';
 
+    setTimeout(() =>{
+    // $('#introBox').hide();
+    $('#introBox').fadeOut('slow');
+
+    $('#portfolio').show();
+    console.log('Done')},
+        4000)
+
+});
